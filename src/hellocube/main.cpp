79d20d95aa512a0660cc052b55c0303cd94f0f5b
ousttrue <ousttrue@gmail.com>
2026-01-28 21:52:54 +0900

    qt6_add_qml_module only work less than cmake-3.19

---
+++ src/hellocube/main.cpp
@@ -0,0 +1,20 @@
+// https://github.com/qt/qtquick3d/blob/dev/examples/quick3d/hellocube
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include <QtGui>
+#include <QtQuick3D/qquick3d.h>
+
+int main(int argc, char *argv[]) {
+  QGuiApplication app(argc, argv);
+
+  QSurfaceFormat::setDefaultFormat(QQuick3D::idealSurfaceFormat());
+
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+  if (engine.rootObjects().isEmpty())
+    return -1;
+
+  return app.exec();
+}
