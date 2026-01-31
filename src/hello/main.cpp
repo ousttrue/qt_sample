9dfb003124f772cc2e0c2f431844f4af9cb84491
ousttrue <ousttrue@gmail.com>
2026-02-01 03:02:41 +0900

    Main.qml

--- src/hello/main.cpp
+++ src/hello/main.cpp
@@ -3,8 +3,19 @@
 
 int main(int argc, char **argv) {
   QGuiApplication app(argc, argv);
-  QUrl source(QStringLiteral("main.qml"));
   QQmlApplicationEngine engine;
-  engine.load(source);
+
+  if (argc > 1) {
+    QFileInfo file(argv[1]);
+    qDebug() << file;
+    if (!file.exists()) {
+      return 1;
+    }
+    engine.load(file.absoluteFilePath());
+  } else {
+    // default
+    engine.loadFromModule("hello", "Main");
+  }
+
   return app.exec();
 }
